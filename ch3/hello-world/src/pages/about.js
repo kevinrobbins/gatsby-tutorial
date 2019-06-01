e4b684bffa91326610a861a20d446f13971dd9e0
Kevin Robbins <kevin.robbins99@gmail.com>
2019-06-01 12:25:42 -0400

    ch3

---
+++ ch3/hello-world/src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+        <h1>About Me</h1>
+        <p>Stuff about me.</p>
+    </Layout>
+)
---
+++ ch3/hello-world/src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+        <h1>I'd love to talk!</h1>
+        <p>
+            <a href="mailto:user@example.com">user@example.com</a>
+        </p>
+    </Layout>
+)
